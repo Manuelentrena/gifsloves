84f96f09811d09fa151cf44276c499fdefea9f45
Manuel Entrena <manuel.entrena@gmail.com>
2021-06-10 11:46:51 +0200

    Arreglamos Error

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Error from "Pages/404/index";
 import { Route } from "wouter";
 import Header from "components/Header";
 import LazyTrending from "components/TrendingSearches";
-import StaticContext from "Provider/StaticContext";
 import { GifsContextProvider } from "Provider/GifsContext";
 import "./App.css";
 
@@ -13,29 +12,22 @@ const Homepage = React.lazy(() => import("./Pages/Home"));
 
 function App() {
   return (
-    <StaticContext.Provider
-      value={{
-        name: "entrena",
-        suscribeiteAlCanal: true,
-      }}
-    >
-      <div className="App">
-        <Header />
-        <Suspense fallback={null}>
-          <section className="App-content">
-            <GifsContextProvider>
-              <Route path="/" component={Homepage} />
-              <Route path="/search/:keyword" component={SearchResults} />
-              <Route path="/gif/:id" component={Detail} />
-              <Route path="/404" component={Error} />
-            </GifsContextProvider>
-            <aside>
-              <LazyTrending />
-            </aside>
-          </section>
-        </Suspense>
-      </div>
-    </StaticContext.Provider>
+    <div className="App">
+      <Header />
+      <Suspense fallback={null}>
+        <section className="App-content">
+          <GifsContextProvider>
+            <Route path="/" component={Homepage} />
+            <Route path="/search/:keyword" component={SearchResults} />
+            <Route path="/gif/:id" component={Detail} />
+            <Route path="/404" component={Error} />
+          </GifsContextProvider>
+          <aside>
+            <LazyTrending />
+          </aside>
+        </section>
+      </Suspense>
+    </div>
   );
 }
 
